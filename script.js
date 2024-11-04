32e7a78a18140f542633f1123e687dd16ace0ddd
Rahyan Ramos <rahyanramos.dev@gmail.com>
2024-11-03 22:08:03 -0400

    Criando função que carrega os gifs na página quando o usuário clica no botão sim

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ $(document).ready(function() {
     let posicaoAnteriorX = 0;
     let posicaoAnteriorY = 0;
     const distanciaMinima = 150;
+    const gifPositions = [];
 
-    $('#btNao').on('mouseover', function() {
-        const container = $('.botoes');
+    $("#btNao").on("mouseover", function() {
+        const container = $(".botoes");
         const larguraContainer = container.width();
         const alturaContainer = container.height();
 
@@ -25,9 +26,49 @@ $(document).ready(function() {
         posicaoAnteriorY = novaPosicaoY;
 
         $(this).css({
-            position: 'absolute',
-            left: novaPosicaoX + 'px',
-            top: novaPosicaoY + 'px'
+            position: "absolute",
+            left: novaPosicaoX + "px",
+            top: novaPosicaoY + "px"
+        });
+    });
+
+    $("#btSim").on("click", function() {
+        $(".pergunta").css("display", "none");
+        $(".botoes").css("display", "none");
+
+        const gifs = [
+            "https://media.tenor.com/O7VXvlDQWbEAAAAi/excited-cat.gif",
+            "https://media.tenor.com/UhqVAq_VjwUAAAAi/squibi-196.gif",
+            "https://media1.tenor.com/m/AgeYHBN9m9EAAAAd/perro-brazil.gif",
+            "https://media1.tenor.com/m/R7wWPCzS9PgAAAAd/shrek-green-lantern.gif",
+            "https://media1.tenor.com/m/kglDzTqoEUAAAAAC/maxwell-cat.gif",
+            "https://media.tenor.com/oyFfNb4Wj_cAAAAi/spongebob-dance-spongebob-joget.gif",
+            "https://media.tenor.com/kQA86PqyXZQAAAAi/small-dancing-white-cat-dance-funny.gif",
+            "https://media.tenor.com/h06DzEZ1neoAAAAi/dance-dog.gif",
+            "https://media.tenor.com/kVLRfEhVYAwAAAAi/fuerza-gabriel-barbosa.gif",
+            "https://media.tenor.com/wu-feCuQpQAAAAAi/neymar-ney.gif"
+        ];
+
+        $("#gifsContainer").empty();
+        gifPositions.length = 0;
+
+        gifs.forEach(gif => {
+            let novaPosicaoX, novaPosicaoY;
+            let posicaoValida = false;
+
+            while (!posicaoValida) {
+                novaPosicaoX = Math.random() * ($(window).width() - 200);
+                novaPosicaoY = Math.random() * ($(window).height() - 200);
+
+                posicaoValida = gifPositions.every(pos => {
+                    const distancia = Math.sqrt(Math.pow(novaPosicaoX - pos.x, 2) + Math.pow(novaPosicaoY - pos.y, 2));
+                    return distancia > distanciaMinima;
+                });
+            }
+
+            gifPositions.push({ x: novaPosicaoX, y: novaPosicaoY });
+
+            $("#gifsContainer").append(`<img src="${gif}" alt="Meme GIF" style="position:absolute; left:${novaPosicaoX}px; top:${novaPosicaoY}px;">`);
         });
     });
 });
